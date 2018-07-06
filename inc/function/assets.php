c6e9046aa8cb7a92f378b38725be5c4db6107397
Paweł Goliński <pawel@whitesite.eu>
2018-07-06 09:00:17 +0200

    cleanup to layout data

--- inc/function/assets.php
+++ inc/function/assets.php
@@ -6,8 +6,9 @@ function load_frontend_assets() {
     wp_enqueue_script('modernizr', fc::i()->av('modernizr', 'modernizr-2.6.2.js'));
     wp_enqueue_script('jquery-full', fc::i()->av('jquery', 'jquery-3.3.1.min.js'));
     wp_enqueue_script('bootstrap', fc::i()->av('bootstrap', 'js' . '/' . 'bootstrap.bundle.min.js'));
+    wp_enqueue_script('wtst', get_template_directory_uri() . '/assets/js/script.js');
 
     wp_enqueue_style('bootstrap', fc::i()->av('bootstrap', 'css' . '/' . 'bootstrap.min.css'));
-    wp_enqueue_style('wtst', get_template_directory_uri() . '/assets/css/wtst.css');
+    wp_enqueue_style('wtst', get_template_directory_uri() . '/assets/css/style.css');
     wp_enqueue_style('main', get_bloginfo('stylesheet_url'));
 }
